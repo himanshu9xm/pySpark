29df17f72967246c9b79bd539dabeaf661aafd71
Himanshu <himanshu9xm@gmail.com>
2023-08-21 03:54:52 +0530

    Housekeeping as promised

--- DataFrames/spark-sql.py
+++ DataFrames/spark-sql.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from pyspark.sql.functions import col
 
+# Create a spark session
 spark = SparkSession.builder.appName("SparkSQL").getOrCreate()
 
 
@@ -20,18 +21,23 @@ try:
     lines = spark.sparkContext.textFile("file:///SparkCourse/pySpark/SampleData/fakefriends.csv")
     people = lines.map(parseLine)
 
+    # Infer the schema, and register the DataFrame as a table.
     schemaPeople = spark.createDataFrame(people).cache()
     schemaPeople.createOrReplaceTempView("people")
 
     #--------------------------Selecting all teenagers i.e people with age from 13 to 19---------------------------------------------------
     # print("With SQL-----------------------------------------")
+    # SQL can be run over DataFrames that have been registered as a table.
     # teenagers = spark.sql("SELECT * FROM people WHERE age>=13 AND age<=19").show()
+    
+    # The results of SQL queries are RDDs and support all the normal RDD operations.
     # results = teenagers.collect()
     # print("ID\tName\tAge\tNumber Of Friends")
     # for result in results:
     #     print(str(result[0]) + "\t" + result[1] + "\t" + str(result[2]) + "\t" + str(result[3]) )
 
     # print("Without SQL-----------------------------------------")
+    # We can also use functions instead of SQL queries:
     # schemaPeople.select("ID", "name", col("age").cast("int"), "numFriends").filter(schemaPeople["age"]>=13).filter(schemaPeople["age"]<=19).orderBy("ID").show()
 
 
